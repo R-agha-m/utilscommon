85ae7d9a58d37158733fa6d731fff9c0add1f5f8
Reza Aghamohammadi <re.agha.mo@gmail.com>
2022-11-02 16:55:40 +0330

    20221102

---
+++ load_parameters.py
@@ -0,0 +1,18 @@
+from os import environ
+
+
+def load_parameters(name,
+                    can_use_config_file=False,
+                    config_file=None,
+                    parent_name_in_config_file=None,
+                    default=None):
+
+    parameter = environ.get(name)
+
+    if (parameter is None) and can_use_config_file:
+        parameter = config_file[parent_name_in_config_file].get(name)
+
+    if parameter is None:
+        parameter = default
+
+    return parameter
