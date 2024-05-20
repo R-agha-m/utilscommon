79fe015233c2088b04939c1e59c30e0395aec7d8
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-05-20 09:43:44 +0330

    20240520

--- utilscommon/setting/schema/boto3.py
+++ utilscommon/setting/schema/boto3.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel
 class SchemaBoto3(BaseModel):
     HOST: str
     PORT: int
-    BUCKET: str
     SERVER_ACCESS_KEY: str
     SERVER_SECRET_KEY: str
     SERVER_ADDRESS_FOR_CLIENT: str
     SCHEDULER_INTERVAL_FOR_DELETE_USELESS_OBJECTS_IN_DAYS: Optional[int] = None
+    EXPIRATION_DURATION_FOR_TEMPORARY_URLS_IN_HOURS: Optional[int] = None
