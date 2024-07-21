486a423870ceb8235a01d8d80ce7a4e96a01782a
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-07-21 14:30:29 +0330

    20240721

--- utilscommon/exception/project_base_exception.py
+++ utilscommon/exception/project_base_exception.py
@@ -7,9 +7,18 @@ class ProjectBaseException(Exception):
                 value,
             )
 
-#             code: int,
+#             status_code: int,
 #             success: bool = None,
 #             data: None | dict | list = None,
 #             error: str | dict | list | None = None,
 #             message: str | dict | list | None = None,
 #             log_this_exc: bool = False
+
+    def __str__(self):
+        return str({
+            self.__class__.__name__: {
+                key: value for key, value in self.__dict__.items() \
+                if not key.startswith('__') \
+                   and not callable(value)
+            }
+        })
