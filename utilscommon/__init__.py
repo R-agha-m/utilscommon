cff09a11f4387ba86c8e81fae779231e169fea0b
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-07-31 18:11:51 +0330

    VERSION is updated to v0.0.3

--- utilscommon/__init__.py
+++ utilscommon/__init__.py
@@ -1,24 +1,11 @@
-try:
-    from utilscommon import logo
-    from utilscommon.setting import *
-    from utilscommon.exception import *
-    from utilscommon.file_and_dir import *
-    from utilscommon.is_test_mode import is_test_mode
-    from utilscommon.make_flat import make_flat
-    from utilscommon.sleep_unil import (
-        sleep_until,
-        async_sleep_until,
-    )
+from utilscommon import logo
+from utilscommon.setting import *
+from utilscommon.exception import *
+from utilscommon.file_and_dir import *
+from utilscommon.is_test_mode import is_test_mode
+from utilscommon.make_flat import make_flat
+from utilscommon.sleep_unil import (
+    sleep_until,
+    async_sleep_until,
+)
 
-
-except ImportError:
-    from utilscommon.utilscommon import logo
-    from utilscommon.utilscommon.setting import *
-    from utilscommon.utilscommon.exception import *
-    from utilscommon.utilscommon.file_and_dir import *
-    from utilscommon.utilscommon.is_test_mode import is_test_mode
-    from utilscommon.utilscommon.make_flat import make_flat
-    from utilscommon.utilscommon.sleep_unil import (
-        sleep_until,
-        async_sleep_until,
-    )
