01f81007a0222b71aca89d53a6e842e0be004e6d
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-07-31 17:38:57 +0330

    VERSION is updated to v0.0.1

--- version.ps1
+++ version.ps1
@@ -2,17 +2,22 @@
 $versionFilePath = "VERSION"
 
 # Check if VERSION exists
-if (-not (Test-Path $versionFilePath)) {
+if (-not (Test-Path $versionFilePath))
+{
     Write-Host "Error: VERSION file not found."
-    exit 1
+    $currentVersion = "v0.0.0"
+    Write-Host "Current version is: $currentVersion"
+}
+else
+{
+    # Read the current version from VERSION
+    $currentVersion = Get-Content -Path $versionFilePath
+    Write-Host "Current version is: $currentVersion"
 }
 
-# Read the current version from VERSION
-$currentVersion = Get-Content -Path $versionFilePath
-Write-Host "Current version is: {1}" -f $currentVersion
-
 # Ensure the version follows the vx.y.z format
-if ($currentVersion -notmatch "^v[0-9]+\.[0-9]+\.[0-9]+$") {
+if ($currentVersion -notmatch "^v[0-9]+\.[0-9]+\.[0-9]+$")
+{
     Write-Host "Error: VERSION does not contain a valid version (vx.y.z format)."
     exit 1
 }
@@ -28,7 +33,7 @@ $patch += 1
 
 # Form the new version
 $newVersion = "v{0}.{1}.{2}" -f $major, $minor, $patch
-Write-Host "New version is: {1}" -f $newVersion
+Write-Host "New version is: $newVersion"
 
 # Write the new version to VERSION
 Set-Content -Path $versionFilePath -Value $newVersion
@@ -37,11 +42,11 @@ Set-Content -Path $versionFilePath -Value $newVersion
 Write-Host "git add --all"
 git add --all
 
-Write-Host "git commit -m 'VERSION is updated to {1}'" -f $newVersion
+Write-Host "git commit -m 'VERSION is updated to $newVersion'"
 git commit -m "VERSION is updated to $newVersion"
 
 # Create the new tag
-Write-Host "git tag {1}'" -f $newVersion
+Write-Host "git tag $newVersion'"
 git tag $newVersion
 
 # Push the changes and the new tag to the server
