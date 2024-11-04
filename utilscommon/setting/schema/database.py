f9cceb2fbbfed30f86f7c59c3a8a1d73bf897bb8
Reza Aghamohammadi <re.agha.mo@gmail.com>
2024-11-04 15:20:08 +0330

    VERSION is updated to v0.0.22

--- utilscommon/setting/schema/database.py
+++ utilscommon/setting/schema/database.py
@@ -4,19 +4,14 @@ from pydantic import BaseModel
 
 
 class SchemaDatabase(BaseModel):
-    CONNECTION_STRING: Optional[str]
-    HOST: str
-    PORT: int
-    DATABASE: str
-    USERNAME: Optional[str]
-    PASSWORD: Optional[str]
-
-
-class SchemaDatabaseWithAuthDb(BaseModel):
-    CONNECTION_STRING: Optional[str]
-    HOST: str
-    PORT: int
-    DATABASE: str
-    USERNAME: Optional[str]
-    PASSWORD: Optional[str]
+    CONNECTION_STRING: Optional[str] = None
+    HOST: Optional[str] = None
+    PORT: Optional[int] = None
+    DATABASE: Optional[str] = None
+    USERNAME: Optional[str] = None
+    PASSWORD: Optional[str] = None
     AUTHDB: Optional[str] = None
+
+
+class SchemaDatabaseWithAuthDb(SchemaDatabase):
+    pass
